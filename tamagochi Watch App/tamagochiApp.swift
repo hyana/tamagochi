25c49526919ac65f53e94fc9f47b8bc773d2d602
hyana <seohyeonhyanakang@gmail.com>
2024-02-20 22:05:24 -0500

    Initial Commit

---
+++ tamagochi Watch App/tamagochiApp.swift
@@ -0,0 +1,17 @@
+//
+//  tamagochiApp.swift
+//  tamagochi Watch App
+//
+//  Created by Hyana Kang on 2/20/24.
+//
+
+import SwiftUI
+
+@main
+struct tamagochi_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
